3f4332bff7de59be2d9ea04d3c3918a984258c7e
tournevis <arthur.ro92@gmail.com>
2015-12-24 18:15:22 +0100

    project card almost rdy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', source:'./images/prototypo.png' , projet:'First project' });
+  res.render('index', { title: 'Express', source:'./images/prototypo.png' , project:'First project' , projectDescript :" Description factice du premier projet "});
 });
 
 router.get('/contact', function(req, res, next) {
