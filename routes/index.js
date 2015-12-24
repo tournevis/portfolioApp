bb8a98faea4b9d4f4c652312b1cd5bd90ea3f77d
tournevis <arthur.ro92@gmail.com>
2015-12-24 16:52:18 +0100

    currently adding project template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', source:'./images/prototypo.png' , projet:'First project' });
 });
 
 router.get('/contact', function(req, res, next) {
