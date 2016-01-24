b34d2465e6480031a278c42efb13eacaf7e56372
tournevis <arthur.ro92@gmail.com>
2016-01-24 16:27:00 +0100

    🐙  update typo

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', source:'./images/prototypo.png' , project:'First project' , projectDescript :" Description factice du premier projet "});
 });
 
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'project' });
+});
+router.get('/exp', function(req, res, next) {
+  res.render('exp', { title: 'exp' });
+});
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'ContactMe' });
 });
 
 
 module.exports = router;
+//https://github.com/midoam2003/alaskapacific.edu/trunk/wp-content/themes/core/fonts/gotham
